6be91ca98da61a6cc6cac3f02bd3bd8e67af853c
Tiffnay Kim <sbtiffanykim@gmail.com>
2021-05-23 23:49:19 +0900

    configure the base template using pug

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -1,11 +1,37 @@
-export const trending = (req, res) => res.send("Trending Videos");
-export const see = (req, res) => {
-  console.log(req.params);
-  return res.send(`Watch Video # ${req.params.id} `);
+export const trending = (req, res) => {
+  const videos = [
+    {
+      title: "video #1",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "video #2",
+      rating: 3,
+      comments: 9,
+      createdAt: "24 minutes ago",
+      views: 5,
+      id: 13,
+    },
+    {
+      title: "video #3",
+      rating: 4.5,
+      comments: 25,
+      createdAt: "12 minutes ago",
+      views: 135,
+      id: 15,
+    },
+  ];
+  return res.render("home", { pageTitle: "Home", videos });
 };
-export const edit = (req, res) => {
-  return res.send(`Edit Video # ${req.params.id}`);
-};
+export const see = (req, res) =>
+  res.render("watch", { pageTitle: "Watch video" });
+export const edit = (req, res) =>
+  res.render("edit", { pageTitle: "Edit video" });
+
 export const deleteVideo = (req, res) => {
   return res.send(`Delete Video # ${req.params.id}`);
 };
