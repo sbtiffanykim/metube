d4b9503a6b1ce2e32081aad434aa3a456fc2c6ca
Tiffnay Kim <sbtiffanykim@gmail.com>
2021-06-04 22:16:14 +0900

    add middlewares to protect pages

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -295,10 +295,10 @@ export const logout = (req, res) => {
 };
 
 export const getEdit = (req, res) => {
-  return res.render("edit-profile", { pageTitle: "Edit Profile" });
-};
-export const postEdit = (req, res) => {
-  return res.render("edit-profile");
+  return res.render("edit-profile", {pageTitle = "Edit Profile"});
 };
 
+export const postEdit = (req, res) => {
+  return res.render("edit-profile");
+}
 export const see = (req, res) => res.send("See User");
