da2f7a0d4b09b5640986353e355b0aa465434095
Tiffnay Kim <sbtiffanykim@gmail.com>
2021-06-04 21:58:59 +0900

    add middlewares to protect pages

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -3,23 +3,25 @@ import {
   callbackGithubLogin,
   callbackKakaoLogin,
   callbackNaverLogin,
-  edit,
+  getEdit,
   logout,
+  postEdit,
   see,
   startGithubLogin,
   startKakaoLogin,
   startNaverLogin,
 } from "../controllers/userControllers";
+import { protectorMiddleware, publicOnlyMiddleware } from "../middlewares";
 
 const userRouter = express.Router();
 
-userRouter.get("/logout", logout);
-userRouter.get("/edit", edit);
-userRouter.get("/github/start", startGithubLogin);
+userRouter.get("/logout", protectorMiddleware, logout);
+userRouter.route("/edit").all(protectorMiddleware).get(getEdit).post(postEdit);
+userRouter.get("/github/start", publicOnlyMiddleware, startGithubLogin);
 userRouter.get("/github/callback", callbackGithubLogin);
-userRouter.get("/kakao/start", startKakaoLogin);
+userRouter.get("/kakao/start", publicOnlyMiddleware, startKakaoLogin);
 userRouter.get("/kakao/callback", callbackKakaoLogin);
-userRouter.get("/naver/start", startNaverLogin);
+userRouter.get("/naver/start", publicOnlyMiddleware, startNaverLogin);
 userRouter.get("/naver/callback", callbackNaverLogin);
 userRouter.get("/:id", see);
 
